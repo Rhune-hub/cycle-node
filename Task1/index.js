a35a428b1119869ae795cd77ae752a063fd74042
Rhune <i.kaminsky@mail.ru>
2020-11-21 17:58:14 +0300

    First true version

--- Task1/index.js
+++ Task1/index.js
@@ -5,4 +5,11 @@
     const curDate = document.createTextNode('Сегодня ' + d.toLocaleDateString('ru-RU', options));
     let dateContainer = document.getElementById('date')
     dateContainer.appendChild(curDate);
+
+/** Изменение стиля таблицы */
+    let currentStyleInput = document.getElementById('style-selector');
+    currentStyleInput.addEventListener('change',changeStyle);
+    function changeStyle() {
+         document.getElementById('price-info').className = currentStyleInput.value;
+    }
     
